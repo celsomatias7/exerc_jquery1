3063bef387404308c9b4cd0e0fabe37c91d1aa49
Celso Matias <celsomatias7@gmail.com>
2023-10-31 07:52:16 -0300

    inclusão do toggleClass

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
     
     $('form').on('submit' , function(e){
-        e.preventDefault();
+        e.preventDefault(); /*para impedir que o formulário recarregue */
         const novaTarefa = $('#nome-tarefa').val();
         const novoItem = $(`<li>${novaTarefa}</li>`);
-        $(novoItem).appendTo('ul');
-        $('#nome-tarefa').val('');
+        
+        $(novoItem).appendTo('ul'); /*colocando a nova tarefa na lista */
+        $('#nome-tarefa').val(''); /*esvaziando o campo input do formulário */
+    })
+
+    $('ul').on('click','li',function(e){
+        e.preventDefault();
+        $(this).toggleClass('riscarTarefa') /*para o evento de riscar a tarefa após o clique, primeiro temos que mudar a classe do item, isso é feito com o toggleClass*/
     })
 })
